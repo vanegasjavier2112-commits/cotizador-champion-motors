ce8228267e1d38191bcbe5252bc0f6c60fef3905
vanegasjavier2112-commits <vanegasjavier2112@gmail.com>
2026-08-22 11:16:36 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE="champion-cotizador-v1";
+const ASSETS=["./","./index.html","./manifest.webmanifest","./champion_motors_logo.png"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
+self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(x=>{const copy=x.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return x}).catch(()=>caches.match("./index.html"))))});
